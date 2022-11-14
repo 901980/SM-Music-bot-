7ca8a111d7e26ec152addb9c1528e568d8c6415b
901980 <105021702+901980@users.noreply.github.com>
2022-11-15 00:57:35 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#
+#
+# This file is part of < https://github.com/901980/SM-Music-bot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/901980/SM-Music-bot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+from .config import *
